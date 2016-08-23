27a4a070ec091d509ed89379d57412f89475bd42
GitLabUser <gitlabst@gmail.com>
2016-08-23 08:49:14 +0000

    php mail() 確認

--- mail.php
+++ mail.php
@@ -2,6 +2,8 @@
     $to = "foxydog520@gmail.com";
     $from = $_REQUEST['name'];
     $headers = "Content-type: text/html;From: $from";
+    
+
 
     $fields = array();
     $fields["name"] = $_REQUEST['name'];
@@ -13,5 +15,13 @@
     $body .= 'Email : '.$fields['email']. '<br>';
     $body .= 'Message : '.$fields['message']. '<br>';
 
-    $send = mail($to, $body, $headers);
-
+    // $send = mail($to, $body, $headers);
+    if(mail("$to", "$body", "$headers")):
+   echo "Your Message was sent successfully.";//寄信成功就會顯示的提示訊息
+  else:
+   echo "Your Message was not sent successfully!";//寄信失敗顯示的錯誤訊息
+  endif;
+    
+    
+    
+?>
